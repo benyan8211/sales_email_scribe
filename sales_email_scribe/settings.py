7974c4c0725564e09b71a7c2892c12a786d2291f
Benjamin Yan <benjaminyan@Benjamins-MacBook-Pro.local>
2026-07-30 10:05:34 -0400

    Hide Secret Key

--- sales_email_scribe/settings.py
+++ sales_email_scribe/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g@*55q!_8#p5$gos(1oxo8lze6m$b)an4tgr%wjix1c-&v^0yw'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

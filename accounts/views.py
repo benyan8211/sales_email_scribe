b7d7ba1c07904c51d314a384e885f4499b7cc3d0
Benjamin Yan <benjaminyan@macbookpro.lan>
2026-08-05 17:40:43 -0400

    Revert force activation invalid page

--- accounts/views.py
+++ accounts/views.py
@@ -71,7 +71,7 @@ def activate_view(request, uidb64, token):
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
-        return render(request, 'accounts/activation_invalid.html')
+        return render(request, 'accounts/activation_success.html')
     else:
         return render(request, 'accounts/activation_invalid.html')
 

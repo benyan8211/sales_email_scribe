3a998c43b0ff0508048be96102f3934a628fdd86
Benjamin Yan <benjaminyan@macbookpro.lan>
2026-08-07 14:09:29 -0400

    Submit Feedback for AI Error

--- scribe_prompt/views.py
+++ scribe_prompt/views.py
@@ -95,29 +95,35 @@ def slow_processing_view_with_feedback(request):
                 result = await Runner.run(sales_agent, "Write a sales email")
                 return result.final_output
 
-    company_name = request.session['company_name']
-    product_name = request.session['product_name']
-    sales_email = request.session['sales_email']
-    system_prompt = f"""You are a sales agent working for {company_name}, a company that is trying to sell {product_name}. 
+    fake_response = requests.Response()
+    fake_response.status_code = 500
+    
+    # Raise the HTTPError with the fake response
+    raise requests.exceptions.HTTPError("Simulated HTTP Error", response=fake_response)
 
-    Here is a sales email that you previously wrote:
-    {sales_email}
+    # company_name = request.session['company_name']
+    # product_name = request.session['product_name']
+    # sales_email = request.session['sales_email']
+    # system_prompt = f"""You are a sales agent working for {company_name}, a company that is trying to sell {product_name}. 
 
-    The user wants you to write a new email. This new email should improve upon that previously written email by taking 
-    into account the following feedback:
-    {request.session['feedback_box']}
+    # Here is a sales email that you previously wrote:
+    # {sales_email}
 
-    Be sure to return your response in html. Make the email look aesthetically pleasing. Include the email's subject in a div that is center aligned.
-    Add a line break. Then include the html content within the body tag.
-    """
+    # The user wants you to write a new email. This new email should improve upon that previously written email by taking 
+    # into account the following feedback:
+    # {request.session['feedback_box']}
 
-    sales_email = async_to_sync(execute_sales_agent)(system_prompt)
+    # Be sure to return your response in html. Make the email look aesthetically pleasing. Include the email's subject in a div that is center aligned.
+    # Add a line break. Then include the html content within the body tag.
+    # """
 
-    request.session['sales_email'] = sales_email
+    # sales_email = async_to_sync(execute_sales_agent)(system_prompt)
 
-    return HttpResponse(f"""
-        {sales_email}
-    """)
+    # request.session['sales_email'] = sales_email
+
+    # return HttpResponse(f"""
+    #     {sales_email}
+    # """)
 
 def slow_processing_view(request):
     async def execute_sales_agent(system_prompt):

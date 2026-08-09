815caf5d8ee1e5e5ad5162569a92c16573b64955
Benjamin Yan <benjaminyan@Benjamins-MacBook-Pro.local>
2026-08-09 09:00:23 -0400

    Create first signup test

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,16 +1,13 @@
-from django.test import TestCase, Client
+from django.test import TestCase, Client, RequestFactory
 from django.urls import reverse
 from django.contrib.auth.models import User
 from django.contrib.messages import get_messages
 from unittest.mock import patch
 import requests
 
-LOGIN_URL = reverse('login') 
-
 class LoginViewTests(TestCase):
     def setUp(self):
         self.client = Client()
-        # Ensure 'login' matches the name in your urls.py
         self.url = reverse('login') 
         self.credentials = {'username': 'test@example.com', 'password': 'securepassword123'}
         
@@ -21,7 +18,7 @@ class LoginViewTests(TestCase):
         )
 
     def test_get_request_renders_login_form(self):
-        """GET request should render the form properly."""
+        """Should render the login form properly."""
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'accounts/login.html')
@@ -36,7 +33,7 @@ class LoginViewTests(TestCase):
         mock_user = object() 
         mock_authenticate.return_value = mock_user
 
-        response = self.client.post(LOGIN_URL, data=self.credentials)
+        response = self.client.post(self.url, data=self.credentials)
         
         mock_authenticate.assert_called_once_with(
             username='test@example.com', 
@@ -48,28 +45,28 @@ class LoginViewTests(TestCase):
     def test_empty_username_and_password_failure(self):
         """Empty username and password should give error message."""
         invalid_credentials = {'username': '', 'password': ''}
-        response = self.client.post(LOGIN_URL, data=invalid_credentials)
+        response = self.client.post(self.url, data=invalid_credentials)
         error_dict = response.context['form'].errors.as_data()
         self.assertEqual(error_dict['username'][0].message, "This field is required.")
 
     def test_empty_password_failure(self):
         """A username entered, but a password left empty will result in an error."""
         invalid_credentials = {'username': 'test@example.com', 'password': ''}
-        response = self.client.post(LOGIN_URL, data=invalid_credentials)
+        response = self.client.post(self.url, data=invalid_credentials)
         error_dict = response.context['form'].errors.as_data()
         self.assertEqual(error_dict['password'][0].message, "This field is required.")
 
     def test_user_does_not_exist_failure(self):
         """username that does not exist will give an error."""
         invalid_credentials = {'username': 'user@example.com', 'password': 'myexamplepasswordmy'}
-        response = self.client.post(LOGIN_URL, data=invalid_credentials)
+        response = self.client.post(self.url, data=invalid_credentials)
         error_dict = response.context['form'].errors.as_data()
         self.assertEqual(error_dict['__all__'][0].message, "Invalid email and/or password.")
 
     def test_user_exists_but_password_is_incorrect_failure(self):
         """username that does exist, but password is incorrect will result in error."""
         invalid_credentials = {'username': 'test@example.com', 'password': 'securepasswordx'}
-        response = self.client.post(LOGIN_URL, data=invalid_credentials)
+        response = self.client.post(self.url, data=invalid_credentials)
         error_dict = response.context['form'].errors.as_data()
         self.assertEqual(error_dict['__all__'][0].message, "Invalid email and/or password.")
     
@@ -77,7 +74,7 @@ class LoginViewTests(TestCase):
     def test_http_error_handling(self, mock_authenticate):
         """When HTTPError arises, it should be handled properly."""
         mock_authenticate.side_effect = requests.exceptions.HTTPError()
-        response = self.client.post(LOGIN_URL, data=self.credentials)
+        response = self.client.post(self.url, data=self.credentials)
         
         messages = list(get_messages(response.wsgi_request))
         self.assertEqual(len(messages), 1)
@@ -87,7 +84,7 @@ class LoginViewTests(TestCase):
     def test_connection_error_handling(self, mock_authenticate):
         """When HTTPError arises, it should be handled properly."""
         mock_authenticate.side_effect = requests.exceptions.ConnectionError()
-        response = self.client.post(LOGIN_URL, data=self.credentials)
+        response = self.client.post(self.url, data=self.credentials)
         
         messages = list(get_messages(response.wsgi_request))
         self.assertEqual(len(messages), 1)
@@ -97,7 +94,7 @@ class LoginViewTests(TestCase):
     def test_timeout_error_handling(self, mock_authenticate):
         """When HTTPError arises, it should be handled properly."""
         mock_authenticate.side_effect = requests.exceptions.Timeout
-        response = self.client.post(LOGIN_URL, data=self.credentials)
+        response = self.client.post(self.url, data=self.credentials)
         
         messages = list(get_messages(response.wsgi_request))
         self.assertEqual(len(messages), 1)
@@ -107,8 +104,22 @@ class LoginViewTests(TestCase):
     def test_request_error_handling(self, mock_authenticate):
         """When HTTPError arises, it should be handled properly."""
         mock_authenticate.side_effect = requests.exceptions.RequestException()
-        response = self.client.post(LOGIN_URL, data=self.credentials)
+        response = self.client.post(self.url, data=self.credentials)
         
         messages = list(get_messages(response.wsgi_request))
         self.assertEqual(len(messages), 1)
         self.assertEqual(str(messages[0]), "An unexpected error occurred! Please try again later.")
+
+class SignupViewTests(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.url = reverse('signup')
+    
+    def test_get_request_renders_signup_form(self):
+        """Should render the signup form properly."""
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/signup.html')
+        self.assertEqual(response.context['form'].fields['email'].label, "Email")
+        self.assertEqual(response.context['form'].fields['password1'].label, "Password")
+        self.assertEqual(response.context['form'].fields['password2'].label, "Password confirmation")
